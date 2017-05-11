ac1db06dd209cc7c8cc406c786610f5553168683
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-05-11 11:12:23 -0500

    Update

--- Final_Project_1/Final_Project_1/Source.cpp
+++ Final_Project_1/Final_Project_1/Source.cpp
@@ -5,74 +5,11 @@
 #include<string>
 
 using namespace std;
-int firstChoice()
-{
-	cout << "\n\n" << "No identity. Only Code.";
 
-
-
-
-}
-
-int secondChoice()
-{
-	cout << "\n\n" << "No.";
-
-	cout << "\n\n" << "";
-
-
-
-}
-int houseDeath()
-{
-
-
-}
-
-int theEnd()
-{
-
-
-}
-int randomnessChallenge()
-{
-
-
-}
 int main()
 {
-
-	int done;
-	int yourChoice;
-
-
-
 	
-		cout << "You enter the chamber and hear a voice on intercom,"<< "Please proceed into SCP - 049 cell now." 
-			<< "Any unauthorized speak is not permitted. We have permission to use lethal force if need be.";
 
-		cout <<"\n\n" << "What would you like to ask first? 1 - Hello? , 2 - Can you read this? , 3 - Good Evening :";
-		cin >> yourChoice;
-		
-		if (yourChoice == 1)
-		{
-			cout <<"\n\n" <<"Hello.";
-
-			cout << "\n\n" << "1 - Could you tell me your name?,  2- Are you aware of your surroundings?, 3- Could you tell me where?" << "Enter what you want to say next :";
-				 cin >> yourChoice;
-
-				 if (yourChoice == 1)
-				 {
-					 
-					 firstChoice();
-					
-				 }
-				 if (yourChoice == 2)
-				 {
-					 
-					 secondChoice();
-				 }
-		}
 	
 	}
 	 
