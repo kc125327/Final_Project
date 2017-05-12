7b9632d62783929d1c7f06f7583dca3232df6030
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-05-12 10:32:18 -0500

    Update

--- Final_Project_1/Final_Project_1/Source.cpp
+++ Final_Project_1/Final_Project_1/Source.cpp
@@ -23,17 +23,38 @@ int main()
 
 		if (yourChoice == 1)
 		{
-			cout << " ";
+			cout << "You attacked your opponent. They are damaged. \n\n" << "Your opponent attacked you. Your health has gone down.";
+			cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+			cin >> yourChoice;
+		}
+		
+		if (yourChoice == 2)
+		{
+			cout << "You defended yourself. Your opponent attacked. Your health is the same.";
+			cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+			cin >> yourChoice;
 		}
 
+		if (yourChoice == 3)
+		{
+
+		}
+
+
 	}
+
+
 	if (yourChoice == 2)
 	{
-		cout << "You defended yourself, there was no action from the opponent.";
+		cout << "You defended yourself. Your opponent defended. No health was damaged.";
+		cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+		cin >> yourChoice;
 	}
+
+
 	if (yourChoice == 3)
 	{
-		cout << "You used an item on your opponent. Opponent choose to do nothing, they were damaged.";
+		cout << "You used an item on your opponent. Opponent choose to do nothing, they health has gone down.";
 
 	}
 
