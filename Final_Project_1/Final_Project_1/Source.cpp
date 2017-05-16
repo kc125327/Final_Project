12bfeffd51351ce038c3ff2fbfd283af898b8238
kc125327 <kc125327@students.davenport.k12.ia.us>
2017-05-16 10:41:33 -0500

    update

--- Final_Project_1/Final_Project_1/Source.cpp
+++ Final_Project_1/Final_Project_1/Source.cpp
@@ -7,63 +7,81 @@
 using namespace std;
 
 
-
-int main()
+int attack(int yourChoice)//first choice 1 in story
 {
-
-	int yourChoice;
-
-	cout << "You get to make the first move, 1- Attack, 2- Defend, 3- Item?";
+	int r;
+	cout << "\n\n You attacked your opponent. Opponent chooses to defend. No damage was caused.";
+	cout << "\n\n What is your next move? 1- Attack, 2- Defend, 3- Item?";
 	cin >> yourChoice;
+
 	if (yourChoice == 1)
 	{
-		cout << "You attacked your opponent. Opponent chooses to defend. No damage was caused.";
-		cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+		cout << "\n\n You attacked your opponent. They are damaged. \n\n" << "Your opponent attacked you. Your health has gone down.";
+		cout << "\n\n What is your next move? 1- Attack, 2- Defend, 3- Item?";
 		cin >> yourChoice;
-
-		if (yourChoice == 1)
-		{
-			cout << "You attacked your opponent. They are damaged. \n\n" << "Your opponent attacked you. Your health has gone down.";
-			cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
-			cin >> yourChoice;
-		}
-		
-		if (yourChoice == 2)
-		{
-			cout << "You defended yourself. Your opponent attacked. Your health is the same.";
-			cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
-			cin >> yourChoice;
-		}
-
-		if (yourChoice == 3)
-		{
-
-		}
-
-
 	}
-
 
 	if (yourChoice == 2)
 	{
-		cout << "You defended yourself. Your opponent defended. No health was damaged.";
-		cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+		cout << "\n\n You defended yourself. Your opponent attacked. Your health is the same.";
+		cout << "\n\n What is your next move? 1- Attack, 2- Defend, 3- Item?";
 		cin >> yourChoice;
 	}
 
-
 	if (yourChoice == 3)
 	{
-		cout << "You used an item on your opponent. Opponent choose to do nothing, they health has gone down.";
-
+		cout << "\n\n You used an item. ";
+		cout << "\n\n You choose to use an item. ";
 	}
-
-	
-	}
-	 
-	
+	return r;
+}
 
 
 
+int defend(int yourChoice)
+{
+	cout << "You defended yourself. Your opponent defended. No health was damaged.";
+	cout << "What is your next move? 1- Attack, 2- Defend, 3- Item?";
+	cin >> yourChoice;
+}
 
 
+
+int item(int yourChoice)
+{
+	cout << "You used an item, ";
+}
+int main()
+{
+
+	string chooseAgain;
+	int yourChoice;
+	int countTotalRounds = 0;
+	do{
+		cout << "You get to make the first move, 1- Attack, 2- Defend, 3- Item?";
+		cin >> yourChoice;
+		if (yourChoice == 1)
+		{
+			attack(yourChoice);
+		}
+		if (yourChoice == 2)
+		{
+			defend(yourChoice);
+		}
+		if (yourChoice == 3)
+		{
+			item(yourChoice);
+
+		}
+
+		cout << "Would you like to go again? <<<";
+		++countTotalRounds;
+		cin >> chooseAgain;
+	}while(chooseAgain == "Yes");
+	cout << "You did " << countTotalRounds << "rounds.";
+
+
+
+
+
+}
